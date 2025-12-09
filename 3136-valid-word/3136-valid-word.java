0061fbfd6a0f0900cbe4e931da2f46018eefadfb
Mahima Aravamudhan <68698437+AMahi1998@users.noreply.github.com>
2025-12-08 21:38:53 -0500

    Time: 1 ms (99.62%), Space: 43.4 MB (22.92%) - LeetHub

---
+++ 3136-valid-word/3136-valid-word.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String word) {
+        if (word.length() < 3) return false;
+
+        boolean hasVowel = false;
+        boolean hasConsonant = false;
+
+        String vowels = "aeiouAEIOU";
+
+        for (char c : word.toCharArray()) {
+
+            if (!Character.isDigit(c) && !Character.isLetter(c)) {
+                return false;
+            }
+
+            if (Character.isLetter(c)) {
+                if (vowels.indexOf(c) != -1) {
+                    hasVowel = true;
+                } else {
+                    hasConsonant = true;
+                }
+            }
+        }
+
+        return hasVowel && hasConsonant;  
+    }
+}
